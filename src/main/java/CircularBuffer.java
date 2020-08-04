d32c9d16cb238c5f6fef1503b017f177c4040faf
Chattarin Pardermwong <chattarinpar@mathregit.group>
2020-08-04 09:59:03 +0700

    Refactor-size-getter

--- src/main/java/CircularBuffer.java
+++ src/main/java/CircularBuffer.java
@@ -1,28 +1,36 @@
 public class CircularBuffer {
     private int bufferSize = 10;
     private int bufferSize2 = 10;
-    private String[] buffer = new String[10];
+    private String[] buffer ;
     private int readPointer;
     private int writePointer;
-
-//    public CircularBuffer() {
-//        this.buffer = new String[10];
-//    }
+    //default
+    public CircularBuffer() {
+        this(10);
+//        this.buffer = new String[bufferSize];
+    }
+    //overload
+    public CircularBuffer(int size) {
+        this.bufferSize = size;
+        this.buffer = new String[bufferSize];
+    }
 
     public boolean isEmpty() {
-        return bufferSize==10;
+        return readPointer == writePointer;
+//        return bufferSize==10;
     }
 
     public void writeData(String input) {
         bufferSize -= 1;
         this.buffer[writePointer++]=input;
-        if(writePointer ==10){
+        if(writePointer == bufferSize){
             writePointer = 0;
         }
     }
 
     public boolean isFull() {
-        return bufferSize==0;
+        return writePointer>=bufferSize;
+//        return bufferSize==0;
     }
 
     public String readData() {
@@ -36,4 +44,11 @@ public class CircularBuffer {
         return false;
     }
 
+    public int getBufferSize() {
+        return bufferSize;
+    }
+
+    public int getWritePointer() {
+        return writePointer;
+    }
 }
--- src/test/java/CircularBufferTest.java
+++ src/test/java/CircularBufferTest.java
@@ -3,17 +3,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CircularBufferTest {
+    CircularBuffer cb = new CircularBuffer();
+
+    @Test
+    public void create_new_buffer_with_size(){
+        cb = new CircularBuffer(5);
+
+    }
 
     @Test
     public void create_new_buffer_should_empty(){
-        CircularBuffer cb = new CircularBuffer();
+//        CircularBuffer cb = new CircularBuffer();
         boolean result = cb.isEmpty();//check isEmpty
         assertTrue("Buffer is not empty",result);//return should be True
     }
 
     @Test
     public void create_new_buffer_with_default_size_should_10(){
-        CircularBuffer cb = new CircularBuffer();
+//        CircularBuffer cb = new CircularBuffer();
         for(int i=0;i<10;i++){
             cb.writeData("A"+i);//add
         }
@@ -23,7 +30,7 @@ public class CircularBufferTest {
 
     @Test
     public void write_A_B_to_buffer_should_read_A_B_from_buffer(){
-        CircularBuffer cb = new CircularBuffer();
+//        CircularBuffer cb = new CircularBuffer();
         cb.writeData("A");
         cb.writeData("B");
 //        String result = cb.readData();
@@ -33,11 +40,11 @@ public class CircularBufferTest {
 
     @Test
     public void writeBuffer_should_not_more_than_Buffer(){
-        CircularBuffer cb = new CircularBuffer();
-        for(int i=0;i<10;i++){
+//        CircularBuffer cb = new CircularBuffer();
+        for(int i=0;i< cb.getBufferSize();i++){
             cb.writeData("A"+i);//add
         }
-        for(int i=0;i<10;i++){
+        for(int i=0;i< cb.getWritePointer();i++){
             assertEquals("A"+i,cb.readData());
         }
         assertFalse(cb.isWriteMorethanBuffer());
@@ -45,8 +52,8 @@ public class CircularBufferTest {
 
     @Test
     public void when_buffer_full_B_overwrite_by_A() {
-        CircularBuffer cb = new CircularBuffer();
-        for(int i=0;i<10;i++){
+//        CircularBuffer cb = new CircularBuffer();
+        for(int i=0;i< cb.getBufferSize();i++){
             cb.writeData("A"+i);//add
         }
         cb.writeData("B");
